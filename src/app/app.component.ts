5c899c2dd122487f5b8cd49646034789b0bc58b9
amitbalaji <amtbalaji199@gmail.com>
2020-04-30 01:48:28 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,39 +6,55 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
- public colArray = [];
- public rowArray = [];
- public treasure = [];
- public colums;
+ public columns;
  public rows;
- public treasureInput: string;
  public isStartGame = false;
+public treasureMatrix:string[][]=[];
 
- getValue(event:any) { 
-   if (!event.target.innerHTML) {
-      event.target.innerHTML = this.treasure || '';
+
+initializeMatrix(){
+  for(let i: number = 0; i < this.rows; i++) {
+            this.treasureMatrix[i]=[]
+  for(let j: number = 0; j<this.columns; j++) {
+          this.treasureMatrix[i][j]=''
+            }
+        }
+}
+
+
+ getValue(i,j) { 
+   if(!(this.treasureMatrix[i][j]==='X')){
+      this.treasureMatrix[i][j]='X'
    } else {
-     event.target.innerHTML ='';
+          this.treasureMatrix[i][j]=''
    }
 }
 
  startGame() {
    this.isStartGame = true;
+  
+ }
+getAnswer(i:any,j:any):string{
+let count=0;
+for(let x:number=i-1>=0?i-1:0;x<=(i+1<this.rows?i+1:this.rows-1);x++){
+for(let y:number=j-1>=0?j-1:0;y<=(j+1<this.columns?j+1:this.columns-1);y++){
+if(this.treasureMatrix[x][y]==='X'){
+count++
+}
+}
+}
+  return count.toString()
+}
+
+ getDimension(){
+  for(let i=0;i<this.rows;i++){
+  for(let j=0;j<this.columns;j++){
+  if(!(this.treasureMatrix[i][j]==='X'))
+  this.treasureMatrix[i][j]=this.getAnswer(i,j)  
+    
+  }  
+  }
  }
  
- setDimension(){
-
-    this.colArray = [];
-    this.rowArray = [];
-    let treasureItems = (this.treasure || '').toString().split(',');
-    for(let i = 0; i<this.colums; i++){
-      this.colArray.push(i);
-    }
-    for(let i = 0; i<this.rows; i++){
-      this.rowArray.push(i);
-    }
-
-
-
-  }
+ 
 }
