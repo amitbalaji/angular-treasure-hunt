9b360b19cf8adda27619b012ec6b66199e6a852e
amitbalaji <amtbalaji199@gmail.com>
2020-04-29 17:43:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-syfnfh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-syfnfh)
---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+ public colArray = [];
+ public rowArray = [];
+ public treasure = [];
+ public colums;
+ public rows;
+ public treasureInput: string;
+ public isStartGame = false;
+
+ getValue(event:any) { 
+   if (!event.target.innerHTML) {
+      event.target.innerHTML = this.treasure || '';
+   } else {
+     event.target.innerHTML ='';
+   }
+}
+
+ startGame() {
+   this.isStartGame = true;
+ }
+ submit(){
+   alert(this.treasure);
+ }
+ setDimension(){
+
+    this.colArray = [];
+    this.rowArray = [];
+    let treasureItems = (this.treasure || '').toString().split(',');
+    for(let i = 0; i<this.colums; i++){
+      this.colArray.push(i);
+    }
+    for(let i = 0; i<this.rows; i++){
+      this.rowArray.push(i);
+    }
+
+
+
+  }
+}
