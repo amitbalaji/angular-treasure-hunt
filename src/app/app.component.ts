97605e5613acc0390e668ad0a4e45c64573d8d07
amitbalaji <amtbalaji199@gmail.com>
2020-04-30 05:16:30 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,60 +1,61 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
- public columns;
- public rows;
- public isStartGame = false;
-public treasureMatrix:string[][]=[];
+export class AppComponent {
+  public columns;
+  public rows;
+  public isStartGame = false;
+  public minNum = 1;
+  public treasureMatrix: string[][] = [];
 
 
-initializeMatrix(){
-  for(let i: number = 0; i < this.rows; i++) {
-            this.treasureMatrix[i]=[]
-  for(let j: number = 0; j<this.columns; j++) {
-          this.treasureMatrix[i][j]=''
-            }
-        }
-}
-
-
- getValue(i,j) { 
-   if(!(this.treasureMatrix[i][j]==='X')){
-      this.treasureMatrix[i][j]='X'
-   } else {
-          this.treasureMatrix[i][j]=''
-   }
-}
-
- startGame() {
-   this.isStartGame = true;
-  
- }
-getAnswer(i:any,j:any):string{
-let count=0;
-for(let x:number=i-1>=0?i-1:0;x<=(i+1<this.rows?i+1:this.rows-1);x++){
-for(let y:number=j-1>=0?j-1:0;y<=(j+1<this.columns?j+1:this.columns-1);y++){
-if(this.treasureMatrix[x][y]==='X'){
-count++
-}
-}
-}
-  return count.toString()
-}
-
- getDimension(){
-  for(let i=0;i<this.rows;i++){
-  for(let j=0;j<this.columns;j++){
-  if(!(this.treasureMatrix[i][j]==='X'))
-  this.treasureMatrix[i][j]=this.getAnswer(i,j)  
-    
-  }  
+  initializeMatrix() {
+    for (let i: number = 0; i < this.rows; i++) {
+      this.treasureMatrix[i] = []
+      for (let j: number = 0; j < this.columns; j++) {
+        this.treasureMatrix[i][j] = ''
+      }
+    }
   }
- }
- 
- 
+
+
+  getValue(i, j) {
+    if (!(this.treasureMatrix[i][j] === 'X')) {
+      this.treasureMatrix[i][j] = 'X'
+    } else {
+      this.treasureMatrix[i][j] = ''
+    }
+  }
+
+  startGame() {
+    this.isStartGame = true;
+  }
+  stopGame() {
+    this.isStartGame = false;
+  }
+  getAnswer(i: any, j: any): string {
+    let count = 0;
+    for (let x: number = i - 1 >= 0 ? i - 1 : 0; x <= (i + 1 < this.rows ? i + 1 : this.rows - 1); x++) {
+      for (let y: number = j - 1 >= 0 ? j - 1 : 0; y <= (j + 1 < this.columns ? j + 1 : this.columns - 1); y++) {
+        if (this.treasureMatrix[x][y] === 'X') {
+          count++
+        }
+      }
+    }
+    return count.toString()
+  }
+
+  getDimension() {
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.columns; j++) {
+        if (!(this.treasureMatrix[i][j] === 'X'))
+          this.treasureMatrix[i][j] = this.getAnswer(i, j)
+
+      }
+    }
+  }
 }
